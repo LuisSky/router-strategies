92c02d2446bb1689dc146e45a74038c5cedacceb
LuisSky <luizguilhermeabrantes@gmail.com>
2020-04-25 18:32:42 -0300

    remove unusables files

--- ExpressRouterEstrategyClass/src/products.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.status(200).send('Products List'));
-  
-router.get('/:name', (req, res) => res.status(200).send(`Product info: ${req.params.name}`));
-
-module.exports = router;
--- ExpressRouterEstrategyClass/src/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/', (req, res) => res.status(200).send('Users List'));
-  
-router.get('/:name', (req, res) => res.status(200).send(`Hello ${req.params.name}`));
-
-module.exports = router;
